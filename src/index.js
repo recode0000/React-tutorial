cb0c894efa39cf77da7271a3a25f97dbb1f2d2b1
Rikito Ikeda <rikito_ikeda@commude.co.jp>
2023-04-16 23:22:42 +0900

    [fix] チュートリアル マルバツゲーム

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 厳格モードで行うためのReactライブラリ
+import React, { StrictMode } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// ブラウザと対話するためのReactライブラリ
+import { createRoot } from "react-dom/client";
+
+// コンポーネントのスタイル
+import "./index.css";
+
+// 作成したコンポーネント
+import App from "./App";
+
+//仮想DOMを作成して
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
